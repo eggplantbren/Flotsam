9026c667c3d18870b2306bf109a0e6045c5a0fbe
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-06-03 17:09:16 -0700

    More limits

--- src/DNest3/Limits.cpp
+++ src/DNest3/Limits.cpp
@@ -1,4 +1,5 @@
 #include "Limits.h"
+#include <cmath>
 
 Limits::Limits()
 :isSet(false)
@@ -26,6 +27,16 @@ void Limits::set(const Data& data)
 		tau_min[i] = -data.get_tRange();
 		tau_max[i] =  data.get_tRange();
 		tau_range[i] = tau_max[i] - tau_min[i];
+	}
+
+	logSig_ml_min.resize(data.get_numImages());
+	logSig_ml_max.resize(data.get_numImages());
+	logSig_ml_range.resize(data.get_numImages());
+	for(int i=0; i<data.get_numImages(); i++)
+	{
+		logSig_ml_min[i] = log(1E-2*data.get_yStDev()[i]);
+		logSig_ml_max[i] = log(1E+2*data.get_yStDev()[i]);
+		logSig_ml_range[i] = logSig_ml_max[i] - logSig_ml_min[i];
 	}
 
 	isSet = true;
