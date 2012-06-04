cd0d3df159e80d412273f5550f75869a6cb45ab0
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-06-03 17:17:42 -0700

    More limits

--- src/DNest3/Limits.cpp
+++ src/DNest3/Limits.cpp
@@ -39,6 +39,23 @@ void Limits::set(const Data& data)
 		logSig_ml_range[i] = logSig_ml_max[i] - logSig_ml_min[i];
 	}
 
+	alpha_min = 1.;
+	alpha_max = 2.;
+	alpha_range = alpha_max - alpha_min;
+
+	logSig_qso_min = 0.;
+	logSig_qso_max = 0.;
+	for(int i=0; i<data.get_numImages(); i++)
+	{
+		logSig_qso_min += 1E-2*data.get_yStDev()[i]/data.get_numImages();
+		logSig_qso_max += 1E+2*data.get_yStDev()[i]/data.get_numImages();
+	}
+	logSig_qso_range = logSig_qso_max - logSig_qso_min;
+
+	logTau_qso_min = log(1E-2*data.get_tRange());
+	logTau_qso_max = log(1E+3*data.get_tRange());
+	logTau_qso_range = logTau_qso_max - logTau_qso_min;
+
 	isSet = true;
 }
 
