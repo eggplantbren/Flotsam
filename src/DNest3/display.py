857fc9e447ea7d88bb5edb0e37ae310b621c0541
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-06-05 11:20:55 -0700

    Convergence plots

--- src/DNest3/display.py
+++ src/DNest3/display.py
@@ -2,9 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sample = np.atleast_2d(np.loadtxt('sample.txt'))
+sample_info = np.atleast_2d(np.loadtxt('sample_info.txt'))
 
 for i in xrange(0, sample.shape[1]):
+	plt.subplot(2,1,1)
 	plt.plot(sample[:,i])
 	plt.title(i)
+	plt.subplot(2,1,2)
+	plt.plot(sample_info[:,0], sample[:,i], 'k.', markersize=2)
+	plt.title('Convergence Plot')
 	plt.show()
 
