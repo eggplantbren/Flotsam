02f603ac2d0fbe8b54367157a29a2f1e25a6f503
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-08-28 15:06:10 +1200

    Compare to Sherry

---
+++ src/DNest3/suyu.py
@@ -0,0 +1,50 @@
+from pylab import *
+
+#This system is special
+#in that the three relative time delays between the four images were measured accurately with errors of only a few
+#percent: tAB = 31.5
+#+2.0
+#-1.0 days, tCB = 36.0
+#+1.5
+#-1.5 days,
+#and tDB = 77.0
+#+2.0
+#-1.0 days (Fassnacht et al. 1999, 2002).
+
+# IMAGE ID 0 = A
+# IMAGE ID 1 = B
+# IMAGE ID 2 = C
+# IMAGE ID 3 = D
+
+posterior_sample = loadtxt('posterior_sample.txt')
+
+tAB = posterior_sample[:,4] - posterior_sample[:,5]
+tCB = posterior_sample[:,6] - posterior_sample[:,5]
+tDB = posterior_sample[:,7] - posterior_sample[:,5]
+
+hist(tAB, 50, alpha=0.2, color='b')
+hist(tCB, 50, alpha=0.2, color='r')
+hist(tDB, 50, alpha=0.2, color='g')
+xlabel('Time Delay $\\tau$ (days)')
+show()
+
+print(mean(tAB), std(tAB))
+print(mean(tCB), std(tCB))
+print(mean(tDB), std(tDB))
+
+
+# Find 68% credible intervals
+def summary(theta):
+	s = sort(theta)
+	a = s[int(0.16*len(theta))]
+	b = s[int(0.5*len(theta))]
+	c = s[int(0.84*len(theta))]
+	return (b - a, b, c - b)
+
+print(summary(tAB))
+print(summary(tCB))
+print(summary(tDB))
+
+
+
+
